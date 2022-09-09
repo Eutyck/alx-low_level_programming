f4c32c9ea662d9e3d1e150c58d56cb136672a637
Eutyck <eutychuskuria@gmail.com>
2022-09-09 17:25:19 +0300

    using puts() ti display text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
  * main - print string
- * the programe prints "Programming is like building a multilingual puzzle
- * return o
+ * the program prints "Programming is like building a multilingual puzzle
+ * return 0
  */
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle\n");
-
-
 	return (0); 
 }
 
