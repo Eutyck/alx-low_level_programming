82e530d36fb0e31febd3cab9767a6c04ad9eebb6
Eutyck <eutychuskuria@gmail.com>
2022-09-09 16:15:06 +0300

    printing text using printf() command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main
+ * print a text
+ * return o
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0); /*return 0*/
+}
+
