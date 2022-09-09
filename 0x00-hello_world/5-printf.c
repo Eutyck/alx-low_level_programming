64f570d8d80bbf76f63deaaf33bb1d89cfb7444e
Eutyck <eutychuskuria@gmail.com>
2022-09-09 18:35:30 +0300

    using printf() to display text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main
- * print a text
- * return o
+ * main - print the string in the fuction
+ * description: using the main fuction
+ * return 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art, \n");
 
-        return (0); /*return 0*/
+        return (0); 
 }
 
