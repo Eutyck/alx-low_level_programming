fd98334485afc746b041c256ee27e1e10640777d
Eutyck <eutychuskuria@gmail.com>
2022-09-09 15:40:06 +0300

    displaying text using puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main
+ * print a text
+ */
+int main (void)
+{	
+	char text[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(text);
+
+	return (0);
+}
+
