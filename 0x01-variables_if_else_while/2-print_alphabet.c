e744000e103c2a290900814e58bb23e19e1e492a
Eutyck <eutychuskuria@gmail.com>
2022-09-09 22:37:02 +0300

    show alphabetic letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entrty point
+ *
+ *return: always 0 (success/correct)
+ */
+int main(void)
+{
+	char l;
+	for (l = 'a'; l<= 'z'; l++)
+{
+	putchar(l);
+}
+	putchar('\n');
+	return(0);
+}
+
+
