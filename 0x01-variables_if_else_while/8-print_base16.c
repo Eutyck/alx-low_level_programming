ffe07abb8aff8697968be58d2a2745b24eb82a73
Eutyck <eutychuskuria@gmail.com>
2022-09-12 15:45:02 +0300

    print digit separated bt ,

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- start point
+ * Description: print hexxa decimal
+ * Return: 0
+ */
+int main (void)
+{
+	char d;
+        for (d = '0'; d <= '9'; d++)
+        {
+                putchar (f);
+        }
+	for (d = 'a'; a <= 'f'; d++)
+	{
+		putchar (d);
+	}
+        putchar ('\n');
+        return (0);
+}
+
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-start point
+ * Description:pringrame print 1 to 9
+ * Return : 0
+ */
+int main (void)
+{
+	int c;
+	for (c = 48; c <= 57; c++)
+{
+	putchar (c);
+	if (c != 57);
+	{
+		putchar (',');
+		putchar(' ');
+	}
+}
+	putchar ('\n');
+	return (0);
+	}
