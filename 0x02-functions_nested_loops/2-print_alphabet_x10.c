ab6f00f62d4a3186a40f3eb580ed7de129f8f6bb
Eutyck <eutychuskuria@gmail.com>
2022-09-12 21:22:59 +0300

    display hexadecimals

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-start point
+ * Description: display alphabet characters ten times
+ *
+ * Return: return 0
+ */
+int main (void)
+{
+	int i;
+	for (i = 1; i <= 10; ++i)
+{
+	char c;
+	for (c ='a'; c <= 'z'; ++c)
+	{
+		putchar (c);
+	}
+}
+	putchar('\n');
+	return (0);
+
+}
+
