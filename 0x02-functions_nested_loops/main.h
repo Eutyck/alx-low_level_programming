606b8aa5b9bda49a4675e234ed00d2121f1ff464
Eutyck <eutychuskuria@gmail.com>
2022-09-13 14:17:29 +0300

    main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-int putchar (char c);
+int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int isalpha(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
