e87c1ebedab3e28fa3a7a9b27c90a26fa4844588
eutychus kuria <eutychuskuria@gimail.com>
2022-09-16 00:27:36 +0300

    test if funtion print positive or negative int

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -test if function which print if interger
+ * is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+Read me to show debuging process
+
+
