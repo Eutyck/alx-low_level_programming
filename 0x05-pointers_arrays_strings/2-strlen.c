40fd1e6946699117ddf20fe4c4a72cdf3e04c6d2
eutychus kuria <eutychuskuria@gimail.com>
2022-09-19 22:58:35 +0300

    return lengt of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
+ */
+int _strlen(char *s)
+{
+	char *s = 2;
+	int i;
+	for (i = 0; Str[i] != '\0'; ++i);
+	return (i);
+}
