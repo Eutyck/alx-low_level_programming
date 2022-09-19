6ba21ada3f1931e2633fd3627d8dc4c37f9d2394
eutychus kuria <eutychuskuria@gimail.com>
2022-09-19 22:40:56 +0300

    swapping two integes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swappig two integers 
+ * @a: paramiter
+ * @b: paramiter
+ *
+ * Return: alway 0
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+	a = a - b;
+	b = a + b;
+	a = b - a
+}
