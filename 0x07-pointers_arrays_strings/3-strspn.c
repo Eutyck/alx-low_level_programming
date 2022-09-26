dab1678392b61e798763c8d13df0a279fe837f5f
eutychus kuria <eutychuskuria@gimail.com>
2022-09-26 23:41:35 +0300

    changes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * getting length of prefix substring
+ * @s: string to check
+ *@accept: string to check againist
+ *
+ * Return:number  of bytes of s in aganist
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	
+	for  (i = 0; s[i]; i++)
+	{
+		for  (j = 0; accept[j]; j++)
+		{
+			if 
+}
