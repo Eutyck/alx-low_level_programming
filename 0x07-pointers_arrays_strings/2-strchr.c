cc3d445b33609cc08f172902de919387535227aa
eutychus kuria <eutychuskuria@gimail.com>
2022-09-26 21:04:17 +0300

    locate charcter in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,4 +4,17 @@
  * @s first argument string memory
  * @c seconed aregument 
  *
- * Return:
+ * Return: pointer to spot in s with c or nul
+ */
+char *_strchr(char *s, char c)
+{
+	int 1;
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + 1) == c)
+			return (s + i);
+	}
+	if (*(s + 1) == c)
+		return (s = i);
+	return (0);
+}
