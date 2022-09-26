20f2ca07043b6b5d5cda19770a27e966fbc5d55e
eutychus kuria <eutychuskuria@gimail.com>
2022-09-26 13:46:00 +0300

    copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * coppying  memory addres from src to dest
+ *
+ * @dest first agument char
+ * @src seconf argument char
+ * @n third argumeny  n
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy (dest, src, sizeof(int) * n);
+	return (0);
+}
