beac44fe509958dde0592347d2f57fe2b566d019
Eutychus <eutychuskuriae@gmail.com>
2022-09-27 16:40:53 +0000

    main.h update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /**
- * main.h prototype for the function
- *prototypes are int & char
- */
+* void prototypes()
+* int prototypes()
+*/
+
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -12,5 +15,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
-#endif /*main.h file*/
+#endif /*MAIN_H
