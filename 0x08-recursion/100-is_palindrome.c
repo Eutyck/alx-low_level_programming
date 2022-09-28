c52128c01aa0fd3baa5fcdaba16c0f0c5f99878f
Eutychus <eutychuskuria@gmail.com>
2022-09-28 23:52:16 +0300

    returns if a string is a palidomdrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * is_palindrome - returns the 1 i s is a paliandrome
  * @s: string to be checked 
@@ -8,48 +7,20 @@
  */
 int is_palindrome(char *s)
 {
-	int flag = 1;
+	int r, sum = 0, temp = 0;
 
-	checks(s, 0, _strlen_recursion(s) - 1 &flag);
-	return (flag);
-}
-
-/**
- * is_palindrome - returns the 1 i s is a paliandrome
- * @s: string to be checked
- * @start: start index
- * @end: end index 
- *
- * Return: void
- */
-void check(char *s, int start, int end, int *flag)
+while (*s > 0)
 {
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end + 1, flag);
-	}
+r = *s%10;
+sum = (sum * 10) + r;
+*s = *s/10;
 }
-
-/**
- * is_palindrome - returns the 1 i s is a paliandrome
- * @s: string to be checked 
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
+if (temp == sum)
 {
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
+	return (1);
 }
-
-
+else
+{
+	return (0);
+}
+}
