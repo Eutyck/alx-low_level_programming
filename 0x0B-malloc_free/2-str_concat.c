1ac8235469dce7959dbc438b3ac1c15398ce56bc
Eutychus <eutychuskuria@gmail.com>
2022-10-04 14:48:31 +0300

    concatinating two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * function str_concat -  concatinates s1 to s2 
+ * @s1: first string 
+ * @s2:  second string 
+ *
+ * Return: function  or NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i;
+	int s1_length = strlen(s1);
+	int s2_length = strlen(s2);
+	int size = s1_length + s2_length + 1;
+       	s = malloc(sizeof(char) * size);
+	
+	for (i = 0; i< s1_length; i++)
+		s[i] = s1[i];
+
+	for (i = 0; i< s2_length; i++)
+                s[s1_length + i] = s2[i];
+	s[size - 1] = '\0';
+
+	return (s);
+
+}
