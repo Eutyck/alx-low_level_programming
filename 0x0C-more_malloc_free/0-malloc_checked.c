539e6cb9f38669d4eee66357a5a2b11c6c0e845a
Eutychus <eutychuskuria@gmail.com>
2022-10-06 16:45:50 +0300

    function to allocate memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * malloc_checked - function allocate memory using malloc
+ * malloc_checked -  allocate memory of mem using malloc
  * @mem: pointer to be used
- *
+ *@s: integer ruturn error
  * Return: if malloc failess return 98
  */
 void *malloc_checked(unsigned int b)
