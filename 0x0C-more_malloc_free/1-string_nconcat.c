79bebea62d34dd25a6a9bb9179bd8962d750253e
Eutychus <eutychuskuria@gmail.com>
2022-10-06 19:54:59 +0300

    concatination of two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,12 +4,39 @@
  * string_nconcat - concatinate function
  * @s1: first string
  * @s2: second string
- * @n int for count
+ * @n: number of bytes of s2 to copy
  *
- * Return: if fail return NULL
+ *Return:char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	strncat (s1, s2, n);
-	return (0);
+	unsigned int i, j, k;
+	char *p;
+
+	if (s1 ==NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j < n)
+		j = n;
+	p = malloc(sizeof(char) * (i +j + 1));
+	if (p == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		p[k] = s1[k];
+	for (k = 0; k < j; k++)
+                p[k + i] = s2[k];
+	p[i + j] = '\0';
+	return (p);
 }
+	
